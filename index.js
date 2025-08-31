f8d82612aec1e8eaba4b2a44a1ee962572ab37d4
Saurabh Nagar <saurabhk2890@gmail.com>
2025-08-31 19:53:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import exprees from "express";
+
+const app = exprees();
+
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
